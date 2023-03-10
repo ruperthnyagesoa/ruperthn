0c2d20cd628f266006a9eec77e8c03ba6e6f2b69
Ruperth <ruperthnyagesoa@gmail.com>
2023-03-10 10:02:07 +0300

    Initial Commit

--- service-worker.js
+++ service-worker.js
@@ -28,7 +28,7 @@ workbox.core.clientsClaim();
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
+ * See https://google/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
